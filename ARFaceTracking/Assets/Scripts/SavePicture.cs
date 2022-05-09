a86a16cc8f2e884bbcbc4c11b1e4414f5740dd98
Artyom Sokolov <15012003artem@mail.ru>
2022-05-09 21:04:39 +0500

    Теперь во время фотографии на фотке не видны кнопки

--- ARFaceTracking/Assets/Scripts/SavePicture.cs
+++ ARFaceTracking/Assets/Scripts/SavePicture.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SavePicture : MonoBehaviour
 {
     public bool takingScreenshot = false;
+    [SerializeField] private float timeToHideUI;
     [SerializeField] private GameObject uiToBeHidden;
 
     public void CaptureScreenshot()
@@ -16,13 +17,13 @@ public class SavePicture : MonoBehaviour
     {
         takingScreenshot = true;
         uiToBeHidden.SetActive(false);
-        // uiToBeHidden.transform
         yield return new WaitForEndOfFrame();
         
         Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         ss.Apply();
-
+        yield return new WaitForSeconds(timeToHideUI);
+        
         // Save the screenshot to Gallery/Photos
         string name = string.Format("{0}_Capture{1}_{2}.png", Application.productName, "{0}", System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
         Debug.Log("Permission result: " + NativeGallery.SaveImageToGallery(ss, Application.productName + " Captures", name));
